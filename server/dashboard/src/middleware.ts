f6a34320a8cc27005bfb1041d75f315d1aa98815
diego-saurus <aldiegoid20019@gmail.com>
2026-07-13 04:20:11 +0700

    add log

--- server/dashboard/src/middleware.ts
+++ server/dashboard/src/middleware.ts
@@ -11,6 +11,7 @@ const PUBLIC_PATHS = [
 ];
 
 export async function middleware(request: NextRequest) {
+  console.log(process.env);
   const { pathname } = request.nextUrl;
 
   if (PUBLIC_PATHS.some((p) => pathname.startsWith(p))) {
