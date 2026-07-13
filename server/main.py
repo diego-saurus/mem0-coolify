866a756056ae56de569bd73eb8bb24a10fce7f58
diego-saurus <aldiegoid20019@gmail.com>
2026-07-14 00:40:41 +0700

    feat: add embedding_model_dims options

--- server/main.py
+++ server/main.py
@@ -129,6 +129,7 @@ DEFAULT_CONFIG = {
             "user": POSTGRES_USER,
             "password": POSTGRES_PASSWORD,
             "collection_name": POSTGRES_COLLECTION_NAME,
+            "embedding_model_dims": EMBEDDING_DIMENSIONS,
         },
     },
     "llm": {
